c6d5c6ab55da3c35df4224f7f22bfa4d0e663eea
jesus-dayo <jesus_dayo@yahoo.com>
2020-05-17 17:21:33 +0800

    3rd challenge - add border

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,15 @@
 function addBorder(picture: string[]): string[] {
-
+    const asterisk: string = '*';
+    const result: string[] = [];
+    const strLength: number = picture[0].length + 2;
+    picture.forEach((p,i)=>{
+        let newStr = asterisk.concat().concat(p).concat(asterisk);
+        result.push(newStr);
+    });
+    const frame = asterisk.repeat(strLength);
+    result.unshift(frame)
+    result.push(frame);
+    return result;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
