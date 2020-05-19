595146d2117abc5c2324ba43e5a5954ed0d8dcfd
jesus-dayo <jesus_dayo@yahoo.com>
2020-05-19 10:30:24 +0800

    4th challenge - addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,11 @@
 function addTwoDigits(n: any): number {
-
+    let sum = 0;
+    while(n != 0){
+        let num = n % 10;
+        sum = sum + num;
+        n = Math.floor(n/10);
+    }
+    return sum;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
