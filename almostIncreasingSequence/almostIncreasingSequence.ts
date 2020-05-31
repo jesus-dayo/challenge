825904f45c53be55f28abbb532abd565e6e7503d
jesus-dayo <jesus_dayo@yahoo.com>
2020-05-31 20:08:10 +0800

    7th challenge - almost increasing sequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,20 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+  let count = 0;
+  let previous = 0;
+  sequence.forEach((value,index)=>{
+     if(count > 1){
+         return false;
+     }
+     if(value < previous){
+         count++;
+     }else{
+         previous = value;
+     }
+  });
 
+  return count < 2;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 1, 1]))
+console.log(almostIncreasingSequence([1, 3, 2, 1]))
+console.log(almostIncreasingSequence([1, 3, 2]))
