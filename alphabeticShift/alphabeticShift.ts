42644bc1313eef01272da8fbf9cb0f25705f3fa3
jesus-dayo <jesus_dayo@yahoo.com>
2020-06-07 13:08:39 +0800

    8th challenge - alphabetic shift - refactor with typescript

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,6 +1,6 @@
 function alphabeticShift(inputString: string): string {
-    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    let splittedStr = inputString.split('');
+    const alphabet: string[] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let splittedStr: string[] = inputString.split('');
     return splittedStr.map(v=>alphabet.indexOf(v)==alphabet.length-1?alphabet[0]:alphabet[alphabet.indexOf(v)+1]).join();
 }
 
