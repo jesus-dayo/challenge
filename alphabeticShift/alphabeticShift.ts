3285c433ca3450df2269598c30a8464b09a85fe0
jesus-dayo <jesus_dayo@yahoo.com>
2020-06-07 13:06:29 +0800

    8th challenge - alphabetic shift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,7 @@
 function alphabeticShift(inputString: string): string {
-
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let splittedStr = inputString.split('');
+    return splittedStr.map(v=>alphabet.indexOf(v)==alphabet.length-1?alphabet[0]:alphabet[alphabet.indexOf(v)+1]).join();
 }
 
 console.log(alphabeticShift('crazy'));
