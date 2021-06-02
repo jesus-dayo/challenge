0ec8d5e0a9902775d41cdd4632443a7bc203bc77
jesus-dayo <jesus_dayo@yahoo.com>
2021-06-03 07:40:54 +0800

    18th challenge - alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,13 @@
 function alphabetSubsequence(s: string): boolean {
-
+    let prev = s.charCodeAt(0);
+    for(let i = 1;i < s.length;i++){
+        let newValue = s.charCodeAt(i);
+        if(newValue <= prev){
+            return false;
+        }
+        prev = newValue;
+    }
+    return true;
 }
 
 console.log(alphabetSubsequence('zab'))
